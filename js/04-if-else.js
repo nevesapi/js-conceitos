c86e3da1c5df69e9272949c801518a618006a1f3
Denis Neves <nevesdenis35@gmail.com>
2025-09-02 14:48:25 -0300

    estrutura condicional switch

--- js/04-if-else.js
+++ js/04-if-else.js
@@ -9,14 +9,14 @@
  }
 
 
- // condicional composta
-if (condição) {
-  console.log("Você é maior de idade.");
-} else {
-  console.log("Você é menor de idade");
-}
+  
+  if (condição) {
+    console.log("Você é maior de idade.");
+  } else {
+    console.log("Você é menor de idade");
+  }
 
-else - senão (se a condição não for atendida, executa o código dentro do else. Ele é executado por último caso nenhuma condição seja atendida)
+  else - senão (se a condição não for atendida, executa o código dentro do else. Ele é executado por último caso nenhuma condição seja atendida)
 
 */
 
---
+++ js/05-switch-case.js
@@ -0,0 +1,72 @@
+// Conhecendo a estrutura condicional switch(troca)
+// Bem similar ao if/else. É utilizada para testar multiplos valores de uma mesma variavel ou expressão
+
+/* 
+ sintaxe básica do switch
+ break - serve para parar a execução do código
+ default - Só será executado caso nenhuma condição seja atendida
+
+ switch(expressão/condição){
+  case valor1:
+    // código se a expressão === valor1
+    break;
+  case valor2:
+    // código se a expressão === valor2
+    break;
+
+  default:
+    // código que vai ser executado caso nenhuma das condições acima seja atendida
+ }
+*/
+
+// exemplo de switch com dias da semana
+
+// altere o valor da variavel dia e veja o switch em ação
+let dia = 4;
+
+switch (dia) {
+  case 1:
+    console.log("domingo");
+    break;
+
+  case 2:
+    console.log("segunda-feira");
+    break;
+
+  case 3:
+    console.log("terça-feira");
+    break;
+
+  case 4:
+    console.log("quarta-feira");
+    break;
+
+  default:
+    console.log("Dia inválido");
+}
+
+// exemplo de switch com operações matemáticas
+let operacao = "+";
+let a = 10;
+let b = 5;
+
+switch (operacao) {
+  case "+":
+    console.log(a + b);
+    break;
+
+  case "-":
+    console.log(a - b);
+    break;
+
+  case "*":
+    console.log(a * b);
+    break;
+
+  case "/":
+    console.log(a / b);
+    break;
+
+  default:
+    console.log("Operação inválida");
+}
